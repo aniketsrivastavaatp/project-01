1aadf73936df44654b3fa785312d32dfd317a361
aniketsrivastavaatp <91611699+aniketsrivastavaatp@users.noreply.github.com>
2021-12-24 15:45:46 +0530

    Add files via upload

---
+++ program_01.py
@@ -0,0 +1,14 @@
+fname = input('Enter file name: ')
+fh = open(fname)
+counts = dict()
+for line in fh:
+    words = line.split()
+    for word in words:
+        counts[word] = counts.get(word, 0) + 1
+bigcount = None
+bigword = None
+for word, count in counts.items():
+    if bigcount is None or count > bigcount:
+        bigword = word
+        bigcount = count
+print(bigword, bigcount)
